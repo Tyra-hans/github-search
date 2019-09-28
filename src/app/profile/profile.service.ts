bece5d77764af62610ddd1199c1cb07e4511c6d1
Tyra-hans <tyrahans17@gmail.com>
2019-09-28 15:06:55 +0300

    add button and input to search on app.component.htm and defined a function bound to the button that logs into console to check if it is working

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -9,15 +9,14 @@ import { map } from 'rxjs/operators';
 export class ProfileService {
 
 private username: string ;
-private avatar: 'https://api.github.com/users/Tyra-hans' ;
-private repos: 'https://api.github.com/users/Tyra-hans/repos' ;
+
 
   constructor(private http: HttpClient) {
     console.log('service is working');
     this.username = 'Tyra-hans';
    }
    getProfileInfo() {
-return this.http.get('https://api.github.com/users/' + this.username + '?avatar_url' + this.avatar + '&repos_url' + this.repos)
+return this.http.get('https://api.github.com/users/' + this.username )
 .pipe(map((response: any) => response.json()));
    }
 }
