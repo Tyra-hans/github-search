3ef73191bc5a9d1f9d306a736fa0bb5a9e9b0047
Tyra-hans <tyrahans17@gmail.com>
2019-09-29 09:12:56 +0300

    creates resquest to github api for list of my repositories to display on homepage.component.html

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { Myprofile} from '../myprofile';
-import {Myrepo} from '../myrepo';
+import { Myprofile } from '../myprofile';
+import { Myrepo } from '../myrepo';
 
 @Component({
   selector: 'app-homepage',
@@ -11,13 +11,19 @@ import {Myrepo} from '../myrepo';
 export class HomepageComponent implements OnInit {
   post: any;
   myprofile: Myprofile;
-  myrepo: Myrepo;
+  myrepo: any;
   username: string;
   // tslint:disable-next-line: deprecation
   constructor(private http: Http) {
     http.get('https://api.github.com/users/tyra-hans')
       .subscribe(response => {
         this.post = response.json();
+
+      });
+    http.get('https://api.github.com/users/tyra-hans/repos')
+      .subscribe(response => {
+        this.myrepo = response.json();
+
       });
   }
 
